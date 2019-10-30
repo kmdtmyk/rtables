492ba765b30985173d2d1f726d4dd428f8d5f2e8
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-10-30 19:39:02 +0900

    Add table list

--- app/controllers/rtables/root_controller.rb
+++ app/controllers/rtables/root_controller.rb
@@ -4,5 +4,19 @@ require_dependency 'rtables/application_controller'
 
 module Rtables
   class RootController < ApplicationController
+
+    def index
+      internal_tables = %w(
+        schema_migrations
+        ar_internal_metadata
+        active_storage_attachments
+        active_storage_blobs
+      )
+      table_names = ActiveRecord::Base.connection.tables - internal_tables
+      @models = table_names.map do |table_name|
+        table_name.classify.constantize
+      end
+    end
+
   end
 end
